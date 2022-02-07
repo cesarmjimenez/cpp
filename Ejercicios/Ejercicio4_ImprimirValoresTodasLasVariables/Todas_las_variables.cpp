40539899e4a3b15f30b2619e908dde9db5448db8
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2022-02-07 16:22:50 -0600

    Add files via upload

---
+++ Ejercicios/Ejercicio4_ImprimirValoresTodasLasVariables/Todas_las_variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    bool valorBoolean;
+    valorBoolean = true;
+
+    int valorEntero = 15;
+    double valorDouble = 20.99;
+    string valorString = "Hola como estan";
+    char valorChar = 'B';
+
+    cout << "valorBoolean: " << valorBoolean << endl;
+    cout << "valorEntero: " << valorEntero << endl;
+    cout << "valorDouble: " << valorDouble << endl;
+    cout << "valorString: " << valorString << endl;
+    cout << "valorChar: " << valorChar << endl;
+
+    return 0;
+}
