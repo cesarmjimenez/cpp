b821f3eae5853f1579d5ab919042bac6873e56e7
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2022-03-13 17:35:46 -0600

    Add files via upload

---
+++ Tarea07/factura.h
@@ -0,0 +1,8 @@
+#include <string>
+
+using namespace std;
+//Tuve que utilizar el #include <string> y el using namespace std; porque
+//de otro modo me daba error
+
+void agregarProducto(string descripcion, int cantidad, double precio);
+void imprimirFactura();
