69c994dde22b2a6bd65b24463cfdb9e85e4a7018
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2022-03-19 17:04:28 -0600

    Add files via upload

---
+++ Tarea08/raizCuadrada.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+float raizCuadrada(float numero)
+{
+
+    return sqrt(numero);
+
+}
+
+int main(int argc, char const *argv[])
+{
+    float numeroUsado = 0;
+
+    system("cls");
+    cout << "************************" << endl;
+    cout << "CALCULO DE RAIZ CUADRADA" << endl;
+    cout << "************************" << endl << endl;
+
+    cout << "Ingrese el numero que desea calcular su raiz cuadrada: ";
+    cin >> numeroUsado;
+
+    cout << endl << endl;
+    cout << "La raiz cuadrada de " << numeroUsado <<  " es de: " <<  raizCuadrada(numeroUsado);
+    cout << endl;
+    system("pause");
+    return 0;
+}
