0ed9fa7e4ecdee5352cc3c7e1c2ba6c6c64e5ed7
cesarmjimenez <98713720+cesarmjimenez@users.noreply.github.com>
2022-04-02 14:03:01 -0600

    Add files via upload

---
+++ EjerciciosParte2/Ejercicio45_puntoDeVentaParteVI_Tarea/factura.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+double subtotal;
+double impuesto = 0.15;
+double total = 0;
+string listaProductos;
+
+void agregarProducto(string descripcion, int cantidad, double precio)
+{
+    listaProductos = listaProductos + descripcion + '\n';
+    subtotal = subtotal + (cantidad * precio);
+}
+
+void imprimirFactura()
+{
+
+    system("cls");
+    cout << "*******" << endl;
+    cout << "FACTURA" << endl;
+    cout << "*******" << endl;
+    cout << endl; 
+
+    cout <<"Productos: " << endl;
+    cout << listaProductos;
+    cout << endl;
+
+    cout << "Subtotal: " << subtotal;
+    cout << endl;
+    cout << endl;
+
+    cout << "Impuesto a pagar 15%: " << subtotal * impuesto;
+    cout << endl;
+    cout << endl;
+
+    total = subtotal + (subtotal * impuesto);
+
+    cout << "Total a pagar: " << total;
+    cout << endl;
+    cout << endl;
+
+    system("pause");
+
+}
